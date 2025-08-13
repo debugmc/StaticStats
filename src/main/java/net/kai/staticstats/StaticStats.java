b1fd96570af4a0042bd9b89b4c02542d2ab37d18
ridriss56 <afakerauchkaiser@gmail.com>
2025-08-13 17:40:48 +0200

    -

--- src/main/java/net/kai/staticstats/StaticStats.java
+++ src/main/java/net/kai/staticstats/StaticStats.java
@@ -1,12 +1,17 @@
 package net.kai.staticstats;
 
+import net.kai.staticstats.commands.StatsCommand;
+import net.kai.staticstats.menu.StatsMenu;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StaticStats extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        new StatsMenu(this);
+        new StatsCommand(this).register();
+
 
     }
 
--- src/main/java/net/kai/staticstats/utility/ColorUtil.java
+++ src/main/java/net/kai/staticstats/utility/ColorUtil.java
@@ -1,4 +1,26 @@
 package net.kai.staticstats.utility;
 
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ColorUtil {
+
+    private static final Pattern HEX_PATTERN = Pattern.compile("&#([A-Fa-f0-9]{6})");
+
+    public static String color(String input) {
+        Matcher matcher = HEX_PATTERN.matcher(input);
+        StringBuffer buffer = new StringBuffer();
+
+        while (matcher.find()) {
+            String hex = matcher.group(1);
+            String replacement = ChatColor.of("#" + hex).toString();
+            matcher.appendReplacement(buffer, replacement);
+        }
+
+        matcher.appendTail(buffer);
+        return ChatColor.translateAlternateColorCodes('&', buffer.toString());
+    }
+
 }
